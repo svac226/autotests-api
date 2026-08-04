df16ee1e49a1c2ebe4630c13e3512ba78154a49a
svac226 <svaceslav78@gmail.com>
2026-08-04 19:22:43 +0300

    Реализовал gRPC сервер и клиент

---
+++ grpc_course_client.py
@@ -0,0 +1,21 @@
+import grpc
+
+import course_service_pb2
+import course_service_pb2_grpc
+
+
+def run():
+    with grpc.insecure_channel("localhost:50051") as channel:
+        stub = course_service_pb2_grpc.CourseServiceStub(channel)
+
+        response = stub.GetCourse(
+            course_service_pb2.GetCourseRequest(
+                course_id="api-course"
+            )
+        )
+
+        print(response)
+
+
+if __name__ == "__main__":
+    run()
