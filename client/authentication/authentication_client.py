ce76595e6545f5f39ae05af9ae0565dd8437d02e
svac226 <svaceslav78@gmail.com>
2026-08-25 21:31:34 +0300

    API client

---
+++ client/authentication/authentication_client.py
@@ -0,0 +1,40 @@
+from httpx import Response
+from client.api_client import APIClient
+from typing import TypedDict
+
+class loginReq(APIClient):
+    """
+    Описание структуры запроса на аутентификацию.
+    """
+    email: str
+    password: str
+
+class RefreshRequestDict(TypedDict):
+    """
+        Клиент для работы с /api/v1/authentication
+    """
+    refreshToken: str
+
+
+class AuthenticationClient(APIClient):
+    """
+    Клиент для работы с /api/v1/authentication
+    """
+    def login_api(self,request: dict) -> Response:
+        """
+        Метод выполняет аутентификацию пользователя.
+
+        :param request: Словарь с email и password.
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        return self.client.post("/api/v1/authentication/login", json=request)
+
+    def refresh_token_api(self,request):
+        """
+        Метод обновляет токен авторизации.
+
+        :param request: Словарь с refreshToken.
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        return self.client.post("/api/v1/authentication/refresh", json=request)
+
